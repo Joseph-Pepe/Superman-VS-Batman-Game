6c6123002d607d441cd6a774cb4781ed92dd7106
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2021-03-29 08:44:50 -0400

    Update Laser.java

--- Laser.java
+++ Laser.java
@@ -45,3 +45,58 @@ public class Laser extends ProjectileWeapon
         }
     }
 }
+
+
+/*[Second Approach extending the Mover class] - Different, but not as good as first approach.
+
+public class Laser extends Mover
+{
+   //The damage this bullet will deal
+   public Laser()
+   {
+       life = 50;
+   }
+   public Laser(Vector speed, int rotation)
+   {
+       super(speed);
+       setRotation(rotation);
+       increaseSpeed(new Vector(rotation, 15));
+       life = 50;
+   }
+  
+   //The bullet will damage asteroids if it hits them.
+   //This act() method runs every frame.
+   public void act()
+   {
+       move();
+       hitEnemy();
+   }
+   private void hitEnemy()
+   {
+       if (isTouching(Batman.class))
+       {
+           DC_Universe DC = (DC_Universe)getWorld();
+           DC.addScore(20);
+       }
+       if(life <= 0)
+       {
+           getWorld().removeObject(this);
+       }
+       else
+       {
+           touchPlayer();
+       }
+   }
+   private void touchPlayer()
+   {
+       Batman batman = (Batman) getOneIntersectingObject(Batman.class);
+       if(batman != null){
+           getWorld().removeObject(this);
+       }
+       else{
+           life--;
+       }
+   }
+}
+
+*/
